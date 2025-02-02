b9e660eaeaf85a8034860695e2f8feeeb2593bd3
Joseph Leo <jaleo@usc.edu>
2025-02-02 00:10:20 -0800

    finaaally done with part 2 whew

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -5,9 +5,133 @@
 
 #include "ulliststr.h"
 
-//Use this file to test your ulliststr implementation before running the test suite
+using namespace std;
+
+// use this file to test your ulliststr implementation before running the test suite
 
 int main(int argc, char* argv[])
 {
-
+// creates the new unrolled linked list instance
+ULListStr* list1 = new ULListStr();
+cout << "push back test" << endl;
+// adding to an empty list
+list1->push_back("0");
+cout << "push back to empty list" << endl;
+cout << "current list: ";
+for (int i = 0; i < list1->size(); i++) {
+    cout << list1->get(i) << " ";
+}
+cout << endl;
+cout << "last element: " << list1->back() << endl;
+// adding when there is space available
+for (int i = 1; i <= 9; i++) {
+    list1->push_back(to_string(i));
+}
+cout << "push when there is space available" << endl;
+cout << "current list: ";
+for (int i = 0; i < list1->size(); i++) {
+    cout << list1->get(i) << " ";
+}
+cout << endl;
+cout << "last element: " << list1->back() << endl;
+// adding where there is no space (should return an exception)
+cout << "push when no space left" << endl;
+list1->push_back("7");
+cout << "current list: ";
+for (int i = 0; i < list1->size(); i++) {
+    cout << list1->get(i) << " ";
+}
+cout << endl;
+cout << "last element: " << list1->back() << endl;
+cout << endl;
+cout << "pop back test" << endl;
+// remove multiple items
+for (int i = 0; i < 9; i++) {
+    list1->pop_back();
+}
+cout << "remove while previous items" << endl;
+cout << "current list: ";
+for (int i = 0; i < list1->size(); i++) {
+    cout << list1->get(i) << " ";
+}
+cout << endl;
+cout << "last element: " << list1->back() << endl;
+// remove the last item
+list1->pop_back();
+cout << "removing the last item" << endl;
+cout << "current list: ";
+// remove from an empty list (should return an exception)
+ULListStr* list2 = new ULListStr();
+cout << "remove from empty list" << endl;
+list2->pop_back();
+cout << "current list: ";
+cout << endl;
+cout << "push front test" << endl;
+ULListStr* list3 = new ULListStr();
+// adding to empty list
+list3->push_front("3");
+cout << "add to empty list" << endl;
+cout << "current list: " << endl;
+for (int i = 0; i < list3->size(); i++) {
+    cout << list3->get(i) << " ";
+}
+cout << "first element: " << list3->front() << endl;
+// add when there is no space before first index (should return an exception)
+list3->push_front("18");
+cout << "adding to front where no space" << endl;
+cout << "current list: " << endl;
+for (int i = 0; i < list3->size(); i++) {
+    cout << list3->get(i) << " ";
+}
+cout << "first element: " << list3->front() << endl;
+// add when space is available before first index
+for (int i = 2; i <= 10; i++) {
+    list3->push_front(to_string(i));
+}
+cout << "add to front when space is available" << endl;
+cout << "current list: " << endl;
+for (int i = 0; i < list3->size(); i++) {
+    cout << list3->get(i) << " ";
+}
+cout << "first element: " << list3->front() << endl;
+cout << "test pop front" << endl;
+// remove when multiple items exist
+for (int i = 0; i < 9; i++) {
+    list3->pop_front();
+}
+cout << "remove items when multiple items exist" << endl;
+cout << "current list: " << endl;
+for (int i = 0; i < list3->size(); i++) {
+    cout << list3->get(i) << " ";
+}
+cout << "first element: " << list3->front() << endl;
+// remove the last item when it is the last node also
+list3->pop_front();
+cout << "removing last element in head with no next" << endl;
+cout << "current list: ";
+// remove from an empty list (should throw an exception)
+ULListStr* list3 = new ULListStr();
+cout << "removing from empty list" << endl;
+list3->pop_front();
+cout << "current list: ";
+cout << "test front and back" << endl;
+ULListStr* list4 = new ULListStr();
+list4->push_back("1");
+list4->push_front("2");
+list4->push_back("3");
+list4->push_front("4");
+cout << "current list: ";
+for (int i = 0; i < list4->size(); i++) {
+    cout << list4->get(i) << " ";
+}
+cout << endl;
+cout << "first element: " << list4->front() << endl;
+cout << "last element: " << list4->back() << endl;
+// free allocated memory
+delete list1;
+delete list2;
+delete list3;
+delete list3;
+delete list4;
+return 0;
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -25,7 +25,180 @@ size_t ULListStr::size() const
 }
 
 // WRITE YOUR CODE HERE
-
+std::string const & ULListStr::back() const{
+  // returns the last value in the tail pointer
+  return tail_->val[tail_->last-1];
+}
+std::string const & ULListStr::front() const{
+  // returns the first value in the head pointer
+  return head_->val[head_->first];
+}
+void ULListStr::push_back(const std::string& val){
+  // validation check for if the list is empty + creates a new item
+  if(empty()){
+    head_ = new Item();
+    // tail is head since its the only item
+    tail_ = head_;
+    // updates the indexes
+    head_->first = 0;
+    head_->last = 1;
+    // stores value at 0 index
+    head_->val[0] = val;
+    // updates size once everything else is updated
+    size_++;
+  }
+  // else if the list is not empty...
+  else{
+    // if there's space beyond the last array
+    if(tail_->last < ARRSIZE){
+      // adds the value of the item behind tail to the tail
+      tail_->val[tail_->last] = val;
+      // moves index of tail->last one backwards
+      tail_->last = (tail_->last) + 1;
+      // updates size once everything else is updated
+      size_++;
+    }
+    // case where there's no space
+    else{
+      // creates the new item
+      Item *temp = new Item();
+      // updates the indexes
+      temp->first = 0;
+      temp->last = 1;
+      // stores value at 0 index
+      temp->val[0] = val;
+      // updates the next iteration of temp
+      temp->prev = tail_;
+      tail_->next = temp;
+      tail_ = temp;
+      // updates size once everything else is updated
+      size_++;
+    }
+  }
+}
+void ULListStr::push_front(const std::string& val){
+  // validation check for if the list is empty
+  if(empty()){
+    head_ = new Item();
+    tail_ = head_;
+    // updates the indexes accordingly
+    head_->first = 0;
+    head_->last = 1;
+    // sets the value of head at 0 index to the actual value    
+    head_->val[0] = val;
+    // updates size once everything else is updated
+    size_++;
+  }
+  // case for when the list is not empty
+  else{
+    // if there is space (if first does not start at 0)
+    if(head_->first > 0){
+      // adds the value of the item before head to the head itself (and updates size accordingly)
+      head_->val[(head_->first)-1] = val;
+      head_->first = (head_->first)-1;
+      size_++;
+    }
+    // case with no space
+    else{
+      Item* temp = new Item();
+      // iterates through the array and updates the indexes starting at the end one element at a time
+      temp->first = ARRSIZE-1;
+      temp->last = ARRSIZE;
+      temp->val[temp->first] = val;
+      temp->next = head_;
+      head_->prev = temp;
+      // updates the head pointer and adjusts size accordingly
+      head_ = temp;
+      size_++;
+    }
+  }
+}
+void ULListStr::pop_back(){
+  // validation check for if the list is empty
+  if(empty()){
+    return;
+  }
+  // case where list is not empty
+  else{
+    if(tail_->first + 1 == tail_->last){
+      Item* temp = tail_;
+      // if tail isn't the last item, update indexes and adjust size accordingly
+      if(tail_->prev != nullptr){
+        tail_ = tail_->prev;
+        tail_->next = nullptr;
+        size_--;
+      }
+      // case for when tail is the last object
+      else{
+        tail_ = nullptr;
+        head_ = tail_;
+        size_--;
+      }
+      delete temp; // free memory (good)
+    }
+    else{
+      // moves the last index backwards and updates size accordingly
+      tail_->last = (tail_->last) - 1;
+      size_--;
+    }
+  }
+}
+void ULListStr::pop_front(){
+  // validation check for if the list is empty
+  if(empty()){
+    return;
+  }
+  // case for non-empty list
+  else{
+    // if there's only one item in the head values
+    if(head_->last - 1 == head_->first){
+      // if there's another item after the head pointer
+      Item* temp = head_;
+      if(head_->next != nullptr){
+        head_ = head_->next;
+        head_->prev = nullptr;
+        size_--;
+      }
+      // case where head is the last item
+      else{
+        head_ = nullptr;
+        tail_ = head_;
+        size_--;
+      }
+      // frees memory
+      delete temp;
+    }
+    // if there's more than one item in head
+    else{
+      head_->first = (head_->first) + 1;
+      size_--;
+    }
+  }
+}
+std::string* ULListStr::getValAtLoc(size_t loc) const{
+  // edgecase for when loc exceeds the allotted size
+  if(loc >= size_){
+    return nullptr;
+  }
+  // iterates through the list
+  size_t counter = 0;
+  Item* temp = head_;
+  while(temp != nullptr){
+    // while not the last item go through the array
+    for(size_t i = temp->first; i < temp->last; i++){
+      // validation check to make sure the value at that address isn't empty
+      if(loc == counter){
+        // returns the value at the counter
+        return &(temp->val[i]);
+      }
+      // updates the counter
+      counter++;
+    }
+    // traverses the list
+    temp = temp->next;
+  }
+  return nullptr;
+}
 void ULListStr::set(size_t loc, const std::string& val)
 {
   std::string* ptr = getValAtLoc(loc);
